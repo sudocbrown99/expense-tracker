e3531d46ec4bb21fd876fb747b044b71edd3b66d
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-12-22 15:37:47 -0600

    Clean read and write functions

--- storage.py
+++ storage.py
@@ -2,27 +2,31 @@ import os
 import csv
 
 storage_file = "data/expenses.csv"
-total_expenses = []
 fieldnames = ['id', 'date', 'category', 'description', 'amount']
 
+"""Make sure csv exists to read and write"""
 def initialize_expenses_storage():
     os.makedirs("data", exist_ok=True)
     
     if not os.path.exists(storage_file): 
         print("Existing expense data not found. Generating new storage data.")
         with open(storage_file, 'x') as file:
-            file.write("id,date,category,description,amount\n")
+            expenses_header = ",".join(fieldnames) + "\n"
+            file.write(expenses_header)
 
-def read_expenses_storage(file):
-    with open(storage_file, newline='') as csvfile:
+"""Reads csv and returns current listed expenses"""
+def read_expenses_storage(filepath):
+    with open(filepath, newline='') as csvfile:
         expensereader = csv.DictReader(csvfile)
+        read_expenses = []
         for row in expensereader:
-            total_expenses.append(row)
-        print(total_expenses)
+            read_expenses.append(row)
+        return read_expenses
 
-def write_expenses_storage(list, file):
-    with open(storage_file, mode='w', newline='') as csvfile:
+"""Writes the contents of read_expenses after modification"""
+def write_expenses_storage(expenses, filepath):
+    with open(filepath, mode='w', newline='') as csvfile:
         expensewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         expensewriter.writeheader()
-        expensewriter.writerows(total_expenses)
+        expensewriter.writerows(expenses)
     print("Data uploaded successfully")
